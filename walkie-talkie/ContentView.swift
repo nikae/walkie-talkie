cac68f3f63643de3c89caed12a654a8c33d0bf11
Nika Elashvili <nika.elashvili@gmail.com>
2021-10-02 11:11:09 -0700

    Initial Commit

---
+++ walkie-talkie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  walkie-talkie
+//
+//  Created by Nika Elashvili on 10/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ walkie-talkie/walkie_talkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  walkie_talkieApp.swift
+//  walkie-talkie
+//
+//  Created by Nika Elashvili on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct walkie_talkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
